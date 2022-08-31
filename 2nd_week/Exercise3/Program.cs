233d48b930f8c08f3e63e98e8df1be648205979a
ncuy0110 <nguyenchonuy2001@gmail.com>
2022-09-01 04:00:40 +0700

    done ex3

---
+++ 2nd_week/Exercise3/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Exercise3
+{
+    internal class Program
+    {
+
+        public static bool checkPrime(int n)
+        {
+            if (n == 2 || n == 3) return true;
+
+            for (int i = 2; i * i <= n; i++)
+            {
+                if (n % i == 0)
+                    return false;
+            }
+
+            return true;
+        }
+
+        private static void Main(string[] args)
+        {
+            int n = 0;
+            while (n < 2)
+            {
+                Console.Write("Enter n = ");
+                n = Convert.ToInt32(Console.ReadLine());
+            }
+
+            if (checkPrime(n)) {
+                Console.WriteLine("N is prime number!");
+            } else {
+                Console.WriteLine("N is not a prime number!");
+            }
+        }
+    }
+}
