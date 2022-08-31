8d1a206f58c7bf9937838f72edb68a2d6e064ebb
ncuy0110 <nguyenchonuy2001@gmail.com>
2022-08-31 19:02:17 +0700

    init

---
+++ 2nd_week/Exercise1/Program.cs
@@ -0,0 +1,41 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int a, b;
+        char operation;
+
+        Console.Write("a = ");
+        a = Convert.ToInt32(Console.ReadLine());
+
+
+        Console.Write("b = ");
+        b = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("operation = ");
+        operation = Console.ReadKey().KeyChar;
+        Console.WriteLine();
+
+        switch (operation)
+        {
+            case '+':
+                Console.WriteLine(a + " + " + b + " = " + (a + b));
+                break;
+            case '-':
+                Console.WriteLine(a + " - " + b + " = " + (a - b));
+                break;
+            case '*':
+                Console.WriteLine(a + " * " + b + " = " + a * b);
+                break;
+            case '/':
+                if (b == 0)
+                    Console.WriteLine("Cannot divided by 0!");
+                else
+                    Console.WriteLine(a + " / " + b + " = " + a / b);
+                break;
+            default:
+                Console.WriteLine("This operator is invalid!");
+                break;
+        }
+    }
+}
---
+++ 2nd_week/Exercise2/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Exercise2
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Double a, b, c;
+
+            Console.Write("a = ");
+            a = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("b = ");
+            b = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("c = ");
+            c = Convert.ToDouble(Console.ReadLine());
+
+            Double delta = b * b - 4 * a * c;
+
+            if (delta < 0)
+                Console.WriteLine("The equation has no roots");
+            else if (delta == 0)
+            {
+                Double x = -b / (2.0 * a);
+                Console.WriteLine("The equation has repeat root is " + x);
+            }
+            else
+            {
+                Double x1 = (-b - Math.Sqrt(delta)) / (2.0 * a);
+                Double x2 = (-b + Math.Sqrt(delta)) / (2.0 * a);
+            }
+        }
+    }
+}
