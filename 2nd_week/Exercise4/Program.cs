0c3bdcbcec9562a1803459c04a4fd176b24be6de
ncuy0110 <nguyenchonuy2001@gmail.com>
2022-09-01 04:21:10 +0700

    done ex4

---
+++ 2nd_week/Exercise4/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Exercise4
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int guessNumber, guessTime = 1;
+            Random rnd = new Random();
+
+            int secretNumber = rnd.Next(1, 101);
+
+            while (guessTime <= 7)
+            {
+                Console.Write($"{guessTime}. Guess number(1-100): ");
+                guessNumber = Convert.ToInt32(Console.ReadLine());
+
+                if (guessNumber == secretNumber)
+                {
+                    Console.Write("Congratulations on winning!");
+                    break;
+                }
+                else
+                {
+                    guessTime++;
+
+                    if (guessNumber < secretNumber)
+                        Console.WriteLine("SMALLER");
+                    else
+                        Console.WriteLine("BIGGER");
+                }
+            }
+
+            if (guessTime>7) 
+            Console.WriteLine("LOSE!");
+        }
+    }
+}
